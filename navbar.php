983a0ed380895cd86e2ea063436d3ef7204ef265
Maxime Meyrat <maximemeyrat@orange.fr>
2021-08-03 18:43:32 +0200

    updated navbar

--- navbar.php
+++ navbar.php
@@ -1,8 +1,6 @@
 <header>
-    <div class="name"><span>Maxime Meyrat</span></div>
     <ul class="navbar">
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="projects.php">Projets</a></li>
-        <li><a href="cursus.php">Cursus</a></li>
+        <li><a href="index.php">ACCUEIL</a></li>
+        <li><a href="projects.php">PROJETS</a></li>
     </ul>
 </header>
