229ab50ab36a3a7e3184eea6b5790570fc526360
Maxime Meyrat <maximemeyrat@orange.fr>
2021-08-05 17:09:37 +0200

    merged files

--- index.php
+++ index.php
@@ -5,28 +5,29 @@
 		<meta content="width=device-width, initial-scale=1" name="viewport" />
 		<title>Maxime Meyrat</title>
 		<link rel="icon" type="image/png" href="assets/images/icon.png" sizes="16x16">
-		<link rel="stylesheet" href="assets/style/style_navbar.css">
 		<link rel="stylesheet" href="assets/style/style_index.css">
 		<link rel="stylesheet" href="assets/style/style.css">
 	</head>
 	<body>
 		<?php include 'navbar.php'; ?>
-		<div id="page">
-			<h1>Accueil</h1>
-			<div class="text_container">
-				<h2>Bienvenu(e)</h2>
-			</div>
-			<h1>Me contacter</h1>
-			<div class="contacts_container">
-				<a class="contact_container" href="https://www.linkedin.com/in/maxime-meyrat-516b15194/" target="_blank">
-					<img src="../assets/images/linkedin.svg" alt="">
-				</a>
-				<a class="contact_container" href="https://github.com/mmeyrat" target="_blank">
-					<img src="../assets/images/github.svg" alt="">
-				</a>
-				<a class="contact_container" href="" target="_blank">
-					<img src="../assets/images/mail.svg" alt="">
-				</a>
+		<div id="grid">
+			<div id="page">
+				<h1>Accueil</h1>
+				<div class="text_container">
+					<h2>Bienvenu(e)</h2>
+				</div>
+				<h1>Me contacter</h1>
+				<div class="contacts_container">
+					<a class="contact_container" href="https://www.linkedin.com/in/maxime-meyrat-516b15194/" target="_blank">
+						<img src="../assets/images/linkedin.svg" alt="">
+					</a>
+					<a class="contact_container" href="https://github.com/mmeyrat" target="_blank">
+						<img src="../assets/images/github.svg" alt="">
+					</a>
+					<a class="contact_container" href="" target="_blank">
+						<img src="../assets/images/mail.svg" alt="">
+					</a>
+				</div>
 			</div>
 		</div>
 	</body>
--- projects.php
+++ projects.php
@@ -6,29 +6,30 @@
 		<title>Meyrat Maxime</title>
 		<link rel="icon" type="image/png" href="assets/images/icon.png" sizes="16x16">
 		<link rel="stylesheet" href="assets/style/style_projects.css">
-		<link rel="stylesheet" href="assets/style/style_navbar.css">
 		<link rel="stylesheet" href="assets/style/style.css">
 	</head>
 	<body>
 		<?php include 'navbar.php'; ?>
-		<div id="page">
-			<h1>Mes projets</h1>
-			<div class="projects_container">
-				<a class="project_container" href="projects/earth.html" target="_blank">
-					<img src="../assets/images/earth.png" alt="">
-					<h3>La planète Terre</h3>
-					<p>Une page web contenant une vue 3D de la planète Terre.</p>
-				</a>
-				<a class="project_container" href="" target="_blank">
-					<img src="../assets/images/error.jpg" alt="">
-					<h3>Projet</h3>
-					<p>Lorem ipsum dolor sit amet...</p>
-				</a>
-				<a class="project_container" href="" target="_blank">
-					<img src="../assets/images/error.jpg" alt="">
-					<h3>Projet</h3>
-					<p>Lorem ipsum dolor sit amet...</p>
-				</a>
+		<div id="grid">
+			<div id="page">
+				<h1>Mes projets</h1>
+				<div class="projects_container">
+					<a class="project_container" href="projects/earth.html" target="_blank">
+						<img src="../assets/images/earth.png" alt="">
+						<h3>La planète Terre</h3>
+						<p>Une page web contenant une vue 3D de la planète Terre.</p>
+					</a>
+					<a class="project_container" href="" target="_blank">
+						<img src="../assets/images/error.jpg" alt="">
+						<h3>Projet</h3>
+						<p>Lorem ipsum dolor sit amet...</p>
+					</a>
+					<a class="project_container" href="" target="_blank">
+						<img src="../assets/images/error.jpg" alt="">
+						<h3>Projet</h3>
+						<p>Lorem ipsum dolor sit amet...</p>
+					</a>
+				</div>
 			</div>
 		</div>
 	</body>
