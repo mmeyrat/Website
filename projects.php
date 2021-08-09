5d2ed397fcec1dfbc4844ebf512462742718d2b8
Maxime Meyrat <maximemeyrat@orange.fr>
2021-08-09 22:15:35 +0200

    updated project page

--- projects.php
+++ projects.php
@@ -10,25 +10,24 @@
 	</head>
 	<body>
 		<?php include 'navbar.php'; ?>
-			<div id="page">
-				<h1>Mes projets</h1>
-				<div class="projects_container">
-					<a class="project_container" href="projects/earth.html" target="_blank">
-						<img src="../assets/images/earth.png" alt="">
-						<h3>La planète Terre</h3>
-						<p>Une page web contenant une vue 3D de la planète Terre.</p>
-					</a>
-					<a class="project_container" href="" target="_blank">
-						<img src="../assets/images/error.jpg" alt="">
-						<h3>Projet</h3>
-						<p>Lorem ipsum dolor sit amet...</p>
-					</a>
-					<a class="project_container" href="" target="_blank">
-						<img src="../assets/images/error.jpg" alt="">
-						<h3>Projet</h3>
-						<p>Lorem ipsum dolor sit amet...</p>
-					</a>
-			</div>
+		<div id="page">
+			<div class="projects_container">
+				<a class="project_container" href="projects/earth.html" target="_blank">
+					<img src="../assets/images/earth.png" alt="">
+					<h3>La planète Terre</h3>
+					<p>Une page web contenant une vue 3D de la planète Terre.</p>
+				</a>
+				<a class="project_container" href="" target="_blank">
+					<img src="../assets/images/error.jpg" alt="">
+					<h3>Projet</h3>
+					<p>Lorem ipsum dolor sit amet...</p>
+				</a>
+				<a class="project_container" href="" target="_blank">
+					<img src="../assets/images/error.jpg" alt="">
+					<h3>Projet</h3>
+					<p>Lorem ipsum dolor sit amet...</p>
+				</a>
+		</div>
 		</div>
 	</body>
 </html>
