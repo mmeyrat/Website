8b514d8316fe17bca33bf6f9e0f66d45e1ff18df
Maxime Meyrat <maximemeyrat@orange.fr>
2021-08-12 20:55:56 +0200

    auto height for project container

--- projects.php
+++ projects.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta content="width=device-width, initial-scale=1" name="viewport" />
-		<title>Meyrat Maxime</title>
+		<title>Maxime Meyrat</title>
 		<link rel="icon" type="image/png" href="assets/images/icon.png" sizes="16x16">
 		<link rel="stylesheet" href="assets/style/style.css">
 		<link rel="stylesheet" href="assets/style/style_projects.css">
@@ -20,7 +20,7 @@
 				<a class="project-container" href="" target="_blank">
 					<img src="../assets/images/error.jpg" alt="">
 					<h3>Projet</h3>
-					<p>Lorem ipsum dolor sit amet...</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra augue consequat, tincidunt justo eu, efficitur felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
 				</a>
 				<a class="project-container" href="" target="_blank">
 					<img src="../assets/images/error.jpg" alt="">
