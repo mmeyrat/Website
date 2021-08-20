87e690019e6f6e67ff7c1f2442a579879c3689ee
Maxime Meyrat <maximemeyrat@orange.fr>
2021-08-20 17:23:39 +0200

    fixes + adjustments

--- assets/borgir.js
+++ assets/borgir.js
@@ -3,7 +3,7 @@ var clickCount = 0;
 function showBorgir() {
 	clickCount++;
 
-	if (clickCount == 1) {
+	if (clickCount == 5) {
 		document.getElementById("text-container").innerHTML = "<video controls autoplay><source src='./assets/images/borgir.mp4' type='video/mp4'></video>";
 	}
 }
