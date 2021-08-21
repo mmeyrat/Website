c7668a92aee53898d7578611d78a74f88edd2b77
Maxime Meyrat <maximemeyrat@orange.fr>
2021-08-21 16:57:29 +0200

    removed video file

--- assets/scripts/video.js
+++ assets/scripts/video.js
@@ -3,7 +3,7 @@ var clickCount = 0;
 function showBorgir() {
 	clickCount++;
 
-	if (clickCount == 5) {
+	if (clickCount == 4) {
 		document.getElementById("text-container").innerHTML = "<video autoplay><source src='./assets/images/video.mp4' type='video/mp4'></video>";
 	}
 }
