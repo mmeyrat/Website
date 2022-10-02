b40ff4ffe0b12b0942aca3f9c1d121e9417c3b01
mmeyrat <maximemeyrat@orange.fr>
2022-10-02 18:01:04 +0200

    hide scroll arrow + fix easter egg

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+let clickCount = 0;
+
+function startEasterEgg() {
+	clickCount++;
+
+	if (clickCount >= 6) {
+		let video = `<video autoplay loop>
+						<source src='./assets/images/video.mp4' type='video/mp4'>
+					 </video>`
+		document.getElementsByTagName("body")[0].innerHTML += video;
+	}
+}
+
+function hideScrollArrow() {
+	let arrow = document.getElementById('title').getElementsByTagName("svg")[0];
+
+	if (window.scrollY == 0) {
+		arrow.style.opacity = "1";
+	} else {
+		arrow.style.opacity = "0";
+	}
+}
+
+window.addEventListener('scroll', hideScrollArrow);
